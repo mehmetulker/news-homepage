6956f00670323dea0b6ee819c3a775c99f145254
mehmet ülker <mehmet-ulker@hotmail.com>
2025-10-27 22:10:16 +0300

    feat: add mobile menu toggle script to switch menu icon and update ARIA attributes

---
+++ components/js/script.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector(".mobile-menu-btn");
+const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+const nav = document.querySelector(".navbar");
+
+if (nav.classList.contains("active")) {
+  mobileMenuIcon.src = "./assets/images/icon-menu-close.svg";
+  mobileMenuIcon.alt = "Close Menu Icon";
+  mobileMenuIcon.setAttribute("aria-expanded", "true");
+  mobileMenuIcon.setAttribute("aria-label", "Close Menu");
+} else {
+  mobileMenuIcon.src = "./assets/images/icon-menu.svg";
+  mobileMenuIcon.alt = "Open Menu Icon";
+  mobileMenuIcon.setAttribute("aria-expanded", "false");
+  mobileMenuIcon.setAttribute("aria-label", "Open Menu");
+}
+btn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  if (nav.classList.contains("active")) {
+    mobileMenuIcon.src = "./assets/images/icon-menu-close.svg";
+    mobileMenuIcon.alt = "Close Menu Icon";
+    mobileMenuIcon.setAttribute("aria-expanded", "true");
+    mobileMenuIcon.setAttribute("aria-label", "Close Menu");
+  } else {
+    mobileMenuIcon.src = "./assets/images/icon-menu.svg";
+    mobileMenuIcon.alt = "Open Menu Icon";
+    mobileMenuIcon.setAttribute("aria-expanded", "false");
+    mobileMenuIcon.setAttribute("aria-label", "Open Menu");
+  }
+});
